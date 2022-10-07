971e62279e711d4f4a7f234860bf776d8a7c6df3
MJ-Curtido <manu17pc@gmail.com>
2022-10-07 14:44:49 +0200

    No me pilla el servicio porque es importado

--- src/app/VistaControlador/app.component.ts
+++ src/app/VistaControlador/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Joke } from "../Modelo/joke";
+import { Servicio } from '../Modelo/servicio';
 
 @Component({
   selector: 'joke-list',
@@ -10,10 +11,7 @@ export class JokeListComponent {
   bromas: Joke[];
 
   constructor() {
-    this.bromas = [
-      new Joke("What did the cheese say when it looked in the mirror?", "Hello-Me (Halloumi)"),
-      new Joke("What kind of cheese do you use to disguise a small horse?", "Mask-a-pony (Mascarpone)"),
-      new Joke("A kid threw a lump of cheddar at me", "I thought ‘That’s not very mature’")
-    ]
+    serv:Servicio = new Servicio();
+    this.bromas = null;
   }
 }
